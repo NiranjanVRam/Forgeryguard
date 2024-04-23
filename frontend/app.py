14d43e608aa649c772ab2f8ab0952b406a77888c
Shiras Rahman U <84281609+Shiras-U@users.noreply.github.com>
2024-04-23 21:47:43 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify, send_file, render_template
 from PIL import Image
 import io
+import os
+from werkzeug.utils import secure_filename
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 
 app = Flask(__name__)
 
@@ -19,6 +23,18 @@ def upload_file():
 
     if file:
         try:
+            filename = 'normal-01'  # Set the standard filename
+            extension = os.path.splitext(file.filename)[1]  # Get the file extension from the uploaded file
+            filename += extension
+
+            # Define the path to the testimages directory
+            save_path = os.path.join(app.root_path, '..', 'testimages', filename)
+
+            # Check if a file with the same name already exists and delete it
+            if os.path.exists(save_path):
+                os.remove(save_path)
+
+            file.save(save_path)  # Save the file to the desired directory
             # Open the image directly without using matplotlib
             img = Image.open(file.stream)
             buffer = io.BytesIO()
@@ -27,6 +43,7 @@ def upload_file():
             return send_file(buffer, mimetype='image/png')
         except IOError:
             return jsonify({'error': 'Invalid image file'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
